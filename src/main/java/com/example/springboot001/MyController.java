204a36c5ba9cc7d88a5410545698aa3120958dff
cuigw <guowei.cui@wzwiz.com>
2022-12-05 10:32:23 +0800

    first commit

---
+++ src/main/java/com/example/springboot001/MyController.java
@@ -0,0 +1,16 @@
+package com.example.springboot001;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class MyController {
+
+
+    @GetMapping(value = "/list", name = "获取配置列表")
+    public String list() {
+        return "aaaaaaaa";
+    }
+}
